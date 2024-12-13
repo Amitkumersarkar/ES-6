eeaffbda4be987c8fa6a7ce3decf5678dd73f873
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-13 21:28:54 +0600

    added array of array

--- keys.js
+++ keys.js
@@ -5,3 +5,19 @@ const watch = {
     isSold: true
 };
 console.log(watch);
+//give us all properties name
+const keys = Object.keys(watch);
+//give us all properties value
+const values = Object.values(watch);
+//give us all entries or pair
+const pair = Object.entries(watch);
+console.log(pair);
+
+// array of array , two dimensional array
+
+// [
+//     ['name', 'watch'],
+//     ['color', 'black'],
+//     ['price', 12000],
+//     ['isSold', true]
+// ]
