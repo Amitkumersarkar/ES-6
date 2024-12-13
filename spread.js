da823a52de3275e47b6ecbcca7d2fe0b8c8e1ca1
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-13 20:03:55 +0600

    added updated spread operator

--- spread.js
+++ spread.js
@@ -1,4 +1,4 @@
-// spread operator
+// spread operator (...) this is spread operator
 
 const max = Math.max(6, 20, 40, 50, 2, 10, 100)
 const numbers = [3, 5, 2, 40, 50, 70, 10];
@@ -8,7 +8,20 @@ console.log(arrayMax);
 
 
 // use spread operator to copy 
-const nums = [4, 5, 80, 10];
-const nums2 = nums;
-nums2.push(12);
-console.log(nums);
+// const nums = [4, 5, 80, 10];
+// const nums2 = nums;
+// nums2.push(12);
+// console.log(nums);
+
+// spread operator copy
+// const friend = friends;
+// const dost = [...friends] //copy
+// console.log(dost);
+// friends.push(100);
+// console.log(dost);
+// console.log(friends);
+
+// // advanced
+
+// const num = [...friends, 5555] //add extra elements while copy
+// console.log(num);
