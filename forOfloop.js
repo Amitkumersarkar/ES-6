16c58ac10853bd79560d470dedc831c36b7844f0
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-13 21:42:34 +0600

    added for of loop in diffrenet function

---
+++ forOfloop.js
@@ -0,0 +1,12 @@
+const numbers = [1, 6, 5, 4, 8];
+// for(let i = 0; i<numbers.length; i++){
+
+// }
+// while
+for (const num of numbers) {
+    // console.log(num);
+}
+const student = 'Amit Sarkar';
+for (const char of student) {
+    console.log(char);
+}
