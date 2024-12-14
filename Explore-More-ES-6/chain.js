f7136bd5ce54050f4c44896868c7bdf29d62a9f4
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-15 00:11:57 +0600

    added data access from array objects

---
+++ Explore-More-ES-6/chain.js
@@ -0,0 +1,9 @@
+// data access
+
+const data = [{
+    name: 'Amit Sarkar',
+    id: 283,
+    dept: 'CSE',
+    address: ' Mirpur-6'
+}];
+console.log(data[0].id, data[0].name, data[0].dept, data[0].address);
