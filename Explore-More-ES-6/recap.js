f221440a282694202e30acdb5d43efbf445fec3b
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-14 23:56:41 +0600

    added recap code

---
+++ Explore-More-ES-6/recap.js
@@ -0,0 +1,7 @@
+const a = 24;
+const number = [24, 10, 12]
+const person = {
+    name: 'Amit Sarkar'
+}
+const message = `Hi , ${person.name} has a:${a} access to ${number[2]}`
+console.log(number);
