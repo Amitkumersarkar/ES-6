16726968473ae7cdab8ba4dd2e1cae5eacfb11c1
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-15 01:38:40 +0600

    added more and more map in js

---
+++ Explore-More-ES-6/map4.js
@@ -0,0 +1,8 @@
+const friends = ['amit', 'amrito', 'myself'];
+const lengths = friends.map(friend => friend.length);
+// console.log(lengths);
+
+// if needed to find out 1st index number 
+
+const firstLetter = friends.map(friend2 => friend2[0]);
+console.log(firstLetter);
