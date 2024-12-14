59770558171429e98857ff1431ae698c34145b20
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-15 01:32:48 +0600

    added more map in js

---
+++ Explore-More-ES-6/map3.js
@@ -0,0 +1,8 @@
+//Array map to do one line loop in js
+const numbers = [2, 4, 6, 8, 10];
+const double = numbers.map(num => num * 2);
+// console.log(double);
+const tenBonus = numbers.map(num => num + 10);
+// console.log(tenBonus);
+const halves = numbers.map(num => num / 2);
+console.log(halves);
