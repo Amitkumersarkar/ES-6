1e00ca8097d3fa6d0ef6cfcbd43d4ec6030f0b9b
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-18 02:21:20 +0600

    added new task

---
+++ Explore-More-ES-6/task.js
@@ -0,0 +1,17 @@
+function min(nums) {
+    return Math.min(nums)
+}
+console.log(min([1, 2, 3]));
+
+// task 2
+
+const [a, b] = [1, 2, 3, 4, 45, 5];
+console.log(a + b);
+
+// task 3
+const { x, y, z } = {
+    x: 1,
+    y1: 2,
+    z: 3
+};
+
